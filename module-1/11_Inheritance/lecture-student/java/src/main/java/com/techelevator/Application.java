b8afcbf198e289b3b594d37c50f1052f9bd29d27
Andy <achongs.business@gmail.com>
2021-03-15 07:42:29 -0400

    Pushing 1-11

---
+++ module-1/11_Inheritance/lecture-student/java/src/main/java/com/techelevator/Application.java
@@ -0,0 +1,14 @@
+package com.techelevator;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        // Create a new general auction
+
+        //....
+        //....
+        // This might go on until the auction runs out of time or hits a max # of bids
+
+    }
+}
